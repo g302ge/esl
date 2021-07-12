34b19ec10df861611bd9e74d5c981d69b3a2cc63
leemaster <leemast3r@outlook.com>
2021-07-13 00:16:38 +0800

    fix the outbound model

--- channel.go
+++ channel.go
@@ -1,9 +1,5 @@
 package esl
 
-import (
-	"sync"
-)
-
 // Channel should be thread safe
 // and every client could use this Channel abstraction in multi-tenant system building
 // if the connection need the queue ?
@@ -31,21 +27,19 @@ import (
 // min idle cpu 0.00/99.63
 // Current Stack Size/Max 240K/8192K
 
-
 // Channel implementors ServerChannel ClientChannel
 // see details https://freeswitch.org/confluence/display/FREESWITCH/mod_commands
-type Channels interface {
-
-	// Execute the command async
+type Channel interface {
+	// Execute the command async only in Outbound pattern
 	Execute(application, args, uuid string) (response *Event, err error)
 
 	// Execute the command aync will return the Job-UUID event
 	ExecuteAsync(application, args, uuid string) (response *Event, err error)
 
-	// Api sync execute api
+	// Api sync execute api return api/response
 	Api(command, args string) (response *Event, err error)
 
-	// Bgapi async execute api
+	// Bgapi async execute api return command/reply with Job-UUID
 	Bgapi(command, args, uuid string) (response *Event, err error) // should return the Job-UUID
 
 	// Filter apply a filter on this socket to receive the specific event
@@ -55,63 +49,26 @@ type Channels interface {
 	Close()
 }
 
-
-// FS strange event model 
+// FS strange event model
 // Generaly speaking the Events on Network should be Sequential consistency
 //
-// SendApi -> ReceiveApiResponse 
+// SendApi -> ReceiveApiResponse
 // SendApi -> ReceiveEvent -> ReceiveApiResponse
 //
 // because that the if the same time FS received the SendApi and prepare to execute, also receive INVITE of SIP generated a CHANNEL_EVENT
 // so any sync all should be blocked before the SendApi finish
 //
-// but there is impossible in this ordering 
-// ReceiveApiResponse2 -> ReceiveApiResponse1 
+// but there is impossible in this ordering
+// ReceiveApiResponse2 -> ReceiveApiResponse1
 //
-// In Outbound accepted channel there is nothing to worry about becuase that all the channel is loop in their own goroutine 
+// In Outbound accepted channel there is nothing to worry about becuase that all the channel is loop in their own goroutine
 // But in Inbound pattern everything could be complex
 // if we use the Global lock to sync to keep the Sequential consistency
-// the application which using this library could be inefficiencily 
+// the application which using this library could be inefficiencily
 // so how to handle this situation ?
 // FIXME: should use two inbound connection one to receive the event and one execute command ?
 
 /// TODO: async method use the UUID to handle the fuck model ?
-
-// Channel the sync mode control the event socket 
-type Channel struct{
-	connection
-	sync.Mutex
-}
-
-
-
-
-// OutboundChannel when accept a new connection from FS will derive a new channel
-// Outbound pattern
-// in this pattern anything could be sync because this object will be maintained by the business goroutine
-// there is not thread-safe problem
-type OutboundChannel struct {
-	connection
-	sync.Mutex
-}
-
-// Connect send the connect command to FS
-// Sync method
-func (channel *OutboundChannel) Connect() (err error) {
-	return
-}
-
-// Linger send the linger command to FS
-// Sync method
-func (channel *OutboundChannel) Linger() (err error) {
-	return
-}
-
-// Nolinger send the noliner command to FS
-// Sync method
-func (channel *OutboundChannel) Nolinger() (err error) {
-	return
-}
 
 // ClientChannel is the simple wrapper of the
 // Inbound pattern
--- outbound.go
+++ outbound.go
@@ -1,1 +1,85 @@
 package esl
+
+import (
+	"errors"
+	"fmt"
+	"sync"
+)
+
+// OutboundChannel when accept a new connection from FS will derive a new channel
+// Outbound pattern
+// in this pattern anything could be sync because this object will be maintained by the business goroutine
+// there is not thread-safe problem
+type OutboundChannel struct {
+	connection
+	sync.Mutex
+	reply    chan *Event
+	response chan *Event
+	Events   chan *Event
+}
+
+// TODO: Execute method execute on the outbound channel
+
+// Loop to receive the events
+func (channel *OutboundChannel) Loop() {
+	for {
+		event, err := channel.recv()
+		if err != nil {
+			//TODO: should handle the logic with the EOF
+			errorm(err)
+			break
+		}
+		if event.Type == EslEvent {
+			channel.Events <- event
+		}
+		if event.Type == EslReplyErr || event.Type == EslReplyOk {
+			channel.reply <- event
+		}
+		if event.Type == EslResponseErr || event.Type == EslResponseOk {
+			channel.response <- event
+		} else {
+			break
+		}
+	}
+}
+
+func (channel *OutboundChannel) syncCommand(command string) (err error) {
+	command = fmt.Sprintf("%s\r\n\r\n", command)
+	channel.Lock()
+	err = channel.send(command)
+	if err != nil {
+		channel.Unlock()
+		errorf("%s execute on outbound connection failed %v", command, err)
+		return
+	}
+	defer channel.Unlock()
+	reply := <-channel.reply
+	if reply.Type == EslReplyErr {
+		err = errors.New(reply.Body)
+		return
+	}
+	return
+}
+
+// Connect send the connect command to FS
+// Sync method
+func (channel *OutboundChannel) Connect() (err error) {
+	return channel.syncCommand("connect")
+}
+
+// Answer the inbound call from outbound channel
+func (channel *OutboundChannel) Answer() (err error) {
+	return channel.syncCommand("answer")
+}
+
+// Linger send the linger command to FS
+// Sync method
+func (channel *OutboundChannel) Linger() (err error) {
+	return channel.syncCommand("linger")
+}
+
+// Nolinger send the noliner command to FS
+// Sync method
+func (channel *OutboundChannel) Nolinger() (err error) {
+	return channel.syncCommand("nolinger")
+}
